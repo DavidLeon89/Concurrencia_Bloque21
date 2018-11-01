5e4140511e4b312bf97d884cf8bfbaf8118f13e5
DavidLeon89 <32190929+DavidLeon89@users.noreply.github.com>
2018-11-01 10:16:34 +0000

    solución 1

--- src/principal.java
+++ src/principal.java
@@ -9,11 +9,18 @@ public class principal extends Thread{
 	//usada por los hilos 
 	private static int tam = 8;
 	private static int[] vec = new int[tam];
+	private int ini, fin;
 	
+	public principal(int ini, int fin) {
+		this.ini = ini;
+		this.fin = fin;
+	}
 	
 	
 	public void run() {
-		
+		for(int i = ini; i < fin; i++) {
+			vec[i] *= 10;
+		}
 	}
 	
 	public static void main(String[] args) {
@@ -22,6 +29,20 @@ public class principal extends Thread{
 		for(int i = 0; i < vec.length; i++) {
 			vec[i] = rand.nextInt(10);
 		}
+		
+		//instanciamos dos objetos de la clase principal. 
+		principal h1 = new principal(0, 4);
+		principal h2 = new principal(4, 8);
+		
+		//lanzamos los dos hilos
+		h1.start();
+		h2.start();
+		
+		try {
+			h1.join();
+			h2.join();
+		}catch(Exception ex) {}
+
 		
 		for(int i = 0; i < vec.length; i++) {
 			System.out.print(vec[i]+" ");
